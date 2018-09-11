779adb5c58e947229a795a7954815e62ab27cf5a
Musigwa <pacifique.musigwa@gmail.com>
2018-09-11 22:47:30 +0200

    damour

--- controllers/driver.controller.js
+++ controllers/driver.controller.js
@@ -1,89 +1,81 @@
-const Driver = require('../models/drivers.model');
+const Driver = require("../models/drivers.model");
 
 // Handle index actions
-exports.index = function (req, res) {
-    Driver.get(function (err, drivers) {
-        if (err) {
-            res.json({
-                status: "error",
-                message: err,
-            });
-        }
-        res.json({
-            status: "success",
-            message: "Drivers retrieved successfully",
-            data: drivers
-        });
+exports.index = function(req, res) {
+  Driver.get(function(err, drivers) {
+    if (err) {
+      res.json({
+        status: "error",
+        message: err
+      });
+    }
+    res.json({
+      status: "success",
+      message: "Drivers retrieved successfully",
+      data: drivers
     });
+  });
 };
 // Handle create Driver actions
-exports.new = function (req, res) {
-    var driver = new Driver({
-        names:req.body.names,
-        address : req.body.address,
-        location : req.body.location,
-        telephone : req.body.telephone,
-        statusDriver : req.body.statusDriver
+exports.new = function(req, res) {
+  var driver = new Driver({
+    names: req.body.names,
+    address: req.body.address,
+    location: req.body.location,
+    telephone: req.body.telephone,
+    statusDriver: req.body.statusDriver
+  });
+  driver
+    .save()
+    .then(function(response) {
+      res.status(201).json({ message: "success", driverInfo: response._doc });
+    })
+    .catch(function(err) {
+      console.log(err);
     });
-        driver.save().then(
-            function (response){
-               console.log(response);
-            }
-        ).catch(function(err){
-            console.log(err);
-        });
-// save the driver and check for errors
-    // driver.save(function (err) {
-        // if (err)
-        //     res.json(err);
-        // res.json({
-            // message: 'New Driver created!',
-            // data: driver
-        // });
-    // });
+  //   save the driver and check for errors
 };
 // Handle view contact info
-exports.view = function (req, res) {
-    Driver.findById(req.params.driver_id, function (err, driver) {
-        if (err)
-            res.send(err);
-        res.json({
-            message: 'Driver details loading..',
-            data: driver
-        });
+exports.view = function(req, res) {
+  Driver.findById(req.params.driver_id, function(err, driver) {
+    if (err) res.send(err);
+    res.json({
+      message: "Driver details loading..",
+      data: driver
     });
+  });
 };
 // Handle update driver info
-exports.update = function (req, res) {
-    Driver.findById(req.params.driver_id, function (err, driver) {
-        if (err)
-            res.send(err);
-        driver.names = req.body.names,
-            driver.address = req.body.address,
-            driver.location = req.body.location,
-            driver.telephone = req.body.telephone,
-            driver.status = req.body.status
-// save the contact and check for errors
-        contact.save(function (err) {
-            if (err)
-                res.json(err);
-            res.json({
-                message: 'Driver Info updated',
-                data: driver
-            });
-        });
+exports.update = function(req, res) {
+  Driver.findById(req.params.driver_id, function(err, driver) {
+    if (err) res.send(err);
+    (driver.names = req.body.names),
+      (driver.address = req.body.address),
+      (driver.location = req.body.location),
+      (driver.telephone = req.body.telephone),
+      (driver.status = req.body.status);
+    // save the contact and check for errors
+    contact.save(function(err) {
+      if (err) res.json(err);
+      res.json({
+        message: "Driver Info updated",
+        data: driver
+      });
     });
+  });
 };
 // Handle delete contact
-exports.delete = function (req, res) {
-    Driver.remove({
-        _id: req.params.driver_id
-    }, function (err, driver) {
-        if (err)
-            res.send(err);
-        res.json({
-            status: "success",
-            message: 'Driver deleted'
-        });
-    });
+exports.delete = function(req, res) {
+  Driver.remove(
+    {
+      _id: req.params.driver_id
+    },
+    function(err, driver) {
+      if (err) res.send(err);
+      res.json({
+        status: "success",
+        message: "Driver deleted"
+      });
+    }
+  );
 };
--- models/drivers.model.js
+++ models/drivers.model.js
@@ -2,33 +2,13 @@
 var mongoose = require('mongoose');
 // Setup schema
 var DriverSchema = mongoose.Schema({
-    names: {
-        type: String,
-        required: true,
-        max: 100
-    },
-    address: {
-        type: String,
-        required: true
-    },
-    location: {
-        type: String,
-        required: true
-    },
-    telephone: {
-        type: String,
-        required: true
-    },
-    statusDriver: {
-        type: String,
-        required: true
-    }
+    names: {type: String,required: true,max: 100},
+    address: {type: String,required: true},
+    location: {type: String,required: true},
+    telephone: {type: String,required: true},
+    statusDriver: {type: String,required: true}
 },
-{ 
-    timestamps: true
-}
-
-);
+{ timestamps: true});
 // Export Driver model
 var Driver = module.exports = mongoose.model('Drivers', DriverSchema);
 module.exports.get = function (callback, limit) {
